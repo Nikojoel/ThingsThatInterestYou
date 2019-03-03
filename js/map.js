4730ae5a26af8aa81f533a76d954b8fff58c841f
Nnikkeli <niko-holopainen@hotmail.com>
2019-03-03 11:44:01 +0200

    kartta alku

---
+++ js/map.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([60.170887,24.941531], 14);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
