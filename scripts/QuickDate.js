6e4cd5a75f84e83bba2a3104ac3e26879daac755
Egiboi <43570890+Egiboi@users.noreply.github.com>
2019-03-05 21:55:16 +0200

    pari button, toiminnalisuus

---
+++ scripts/QuickDate.js
@@ -0,0 +1,22 @@
+function setToday() {
+  setDate(0);
+}
+function setTomorrow() {
+  setDate(1);
+}
+function setDate(x) {
+  var d = new Date();
+  console.log(d.getFullYear() + d.getMonth() + d.getDate());
+  var y = d.getFullYear();
+  var m = 1 + d.getMonth();
+  if (m < 10) {
+    m = '0' + m;
+  }
+  var da = d.getDate()+x;
+  if (da < 10) {
+    da = '0' + da;
+  }
+  var date = y + '-' + m + '-' + da;
+  document.getElementById('start_date').value = date;
+  document.getElementById('end_date').value = date;
+}
